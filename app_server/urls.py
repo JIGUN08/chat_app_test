885b32a1d75060222fd6c10bc7a3987a2b2ee0ef
JIGUN08 <skycenter205@gmail.com>
2025-10-24 12:14:27 +0900

    Update urls.py

--- app_server/urls.py
+++ app_server/urls.py
@@ -2,8 +2,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse # 추가
+
+def root_status_view(request):
+    return JsonResponse({"status": "ok", "message": "API is alive (via root)"})
 
 urlpatterns = [
+    path('', root_status_view),
     path('admin/', admin.site.urls),
     # 💡 핵심: api 앱의 모든 URL을 'api/' 경로 아래에 포함
     path('api/', include('api.urls')), 
